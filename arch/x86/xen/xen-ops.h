ba8757b25fbd90dae448a25907d46ad75007a2af
Xin Li (Intel) <xin@zytor.com>
2024-10-29 00:48:58 -0700

    x86/msr: Remove calling native_{read,write}_msr{_safe}() in pmu_msr_{read,write}()
    hpa found that pmu_msr_write() is actually a completely pointless
    function [1]: all it does is shuffle some arguments, then calls
    pmu_msr_chk_emulated() and if it returns true AND the emulated flag
    is clear then does *exactly the same thing* that the calling code
    would have done if pmu_msr_write() itself had returned true.  And
    pmu_msr_read() does the equivalent stupidity.
    
    Remove the calls to native_{read,write}_msr{_safe}() within
    pmu_msr_{read,write}(). Instead reuse the existing calling code
    that decides whether to call native_{read,write}_msr{_safe}() based
    on the return value from pmu_msr_{read,write}().  Consequently,
    eliminate the need to pass an error pointer to pmu_msr_{read,write}().
    
    While at it, refactor pmu_msr_write() to take the MSR value as a u64
    argument, replacing the current dual u32 arguments, because the dual
    u32 arguments were only used to call native_write_msr{_safe}(), which
    has now been removed.
    
    [1]: https://lore.kernel.org/lkml/0ec48b84-d158-47c6-b14c-3563fd14bcc4@zytor.com/
    
    Suggested-by: H. Peter Anvin (Intel) <hpa@zytor.com>
    Sign-off-by: Xin Li (Intel) <xin@zytor.com>

--- arch/x86/xen/xen-ops.h
+++ arch/x86/xen/xen-ops.h
@@ -274,8 +274,8 @@ void xen_pmu_finish(int cpu);
 static inline void xen_pmu_init(int cpu) {}
 static inline void xen_pmu_finish(int cpu) {}
 #endif
-bool pmu_msr_read(unsigned int msr, uint64_t *val, int *err);
-bool pmu_msr_write(unsigned int msr, uint32_t low, uint32_t high, int *err);
+bool pmu_msr_read(const u32 msr, u64 *val);
+bool pmu_msr_write(const u32 msr, const u64 val);
 int pmu_apic_update(uint32_t reg);
 unsigned long long xen_read_pmc(int counter);
 
